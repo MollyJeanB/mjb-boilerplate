9e8571c2d1ebf1a8197b4c43480794d68247aa9c
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2023-04-07 16:37:03 -0700

    [F] Set up initial main page

---
+++ src/components/gitHubLink/GitHubLink.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import styles from "./gitHubLink.module.css"
+
+type Props = {
+	link: string
+}
+
+export const GITHUB_BUTTON_TITLE: string = "Built by MJB 🤸🏻‍♀️"
+
+export const GitHubLink: React.FC<Props> = ({ link }) => {
+	return (
+		<a href={link}>
+			<button className={styles.button} role={"link"}>
+				{GITHUB_BUTTON_TITLE}
+			</button>
+		</a>
+	)
+}
